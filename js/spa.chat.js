3e3cab1cec14d106859a9e388faff9c2f828ac7e
t-sin <shinichi.tanaka45@gmail.com>
2017-05-08 08:35:09 +0900

    Chat module initial commit

---
+++ js/spa.chat.js
@@ -0,0 +1,16 @@
+// chat module
+
+spa.shat = (function () {
+    var configMap = {
+        main_html: String()
+            + '<div style="padding: 1em; color: #fff;">"'
+            + 'say hello to chat'
+            + '</div>',
+        settableMap: {}
+    }
+        stateMap = {$container: null},
+        jqueryMap = {},
+
+        setJqueryMap, configModule, initModule;
+
+});
